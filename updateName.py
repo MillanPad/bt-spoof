c7ed9c3eeb42f04a2297675e4887c13f7a7658b4
MillanPad <73239549+MillanPad@users.noreply.github.com>
2023-05-26 10:45:52 +0200

    Update updateName.py

--- updateName.py
+++ updateName.py
@@ -5,12 +5,10 @@ import shlex
 import fileinput
 import re
 def clone_device(mac_address,nombre,class_of_device):
-    #mac_address = ''.join(c for c in mac_address if c.isalnum())
-    print(mac_address)
     # Cambiar la direccion MAC con bdaddr
     comando_mac = ["sudo", "./bdaddr", "-r",mac_address]
     subprocess.run(comando_mac, capture_output=True)
-
+    # Reinicia el servicio y el adaptador bluetooth para que se realicen los cambios
     comando_restart = ['sudo', 'systemctl', 'restart', 'bluetooth.service']
     subprocess.run(comando_restart, capture_output=True)
 
@@ -23,10 +21,13 @@ def clone_device(mac_address,nombre,class_of_device):
 import dbus
 
 def change_bluetooth_name(new_name):
+    # Mediante dbus obtiene la interfaz bluetooth y el adaptador
     bus = dbus.SystemBus()
     adapter_path = '/org/bluez/hci0'  # Cambia el valor a la ruta correcta de tu adaptador
     adapter = dbus.Interface(bus.get_object('org.bluez', adapter_path), 'org.freedesktop.DBus.Properties')
+    # Cambia el nombre del adaptador obtenido
     adapter.Set('org.bluez.Adapter1', 'Alias', dbus.String(new_name))
 
 def change_class_of_device(new_class):
+    #Ejecuta el comando hciconfig para cambiar el CoD del adaptador
     subprocess.run(['sudo','hciconfig','-a','hci0','class',hex(new_class)],capture_output=True)
